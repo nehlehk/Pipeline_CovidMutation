f1c1fc4cd18a309200a0c6a95116d6647210f3ac
nehlehk <kargarfard870@gmail.com>
2022-11-23 12:24:31 +1100

    starting point of the project

---
+++ bin/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+'''
+this file created by Nehleh to rewrite the conding of this pipeline:
+https://github.com/Carol-Lee-gh/Covid-Mutation-Pipieline
+'''
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
