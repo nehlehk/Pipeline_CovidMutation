04e346346316432cdfae0ca56dee68925d5e2730
nehlehk <kargarfard870@gmail.com>
2022-11-23 16:00:04 +1100

    the initail setups for all files

--- bin/main.py
+++ bin/main.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
-
 '''
-this file created by Nehleh to rewrite the conding of this pipeline:
-https://github.com/Carol-Lee-gh/Covid-Mutation-Pipieline
+this file created by Nehleh 23/11/2022 to rewrite the conding of this pipeline: https://github.com/Carol-Lee-gh/Covid-Mutation-Pipieline
 '''
 
+import extract_mutations
+import Merge
+import plots
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print("================================== help ==================================")
+    print(" to use this pipeline you can use these switches:")
+    print("1)  ")
+    print("2)  ")
+    print("3)  ")
+    print("4)  ")
 
